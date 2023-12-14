f7dec5a1793db9ec7db27e292fe87c5cc1972203
Ashish Yadav <ashishyadav@Ashishs-MacBook-Air.local>
2023-12-14 19:17:48 +0530

    Initialize node js and connect with postgresql

---
+++ readme.js
@@ -0,0 +1,8 @@
+/* 
+    Install Express.js
+    Install Express Gernetor --> To Build the directory and file Structure
+    Inttall Nodman ---> To hot relaod
+
+
+    .Jade is extension to write HTML.
+*/
