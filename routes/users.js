8033c1c7abb06c161f18e6f9e8a635cc8a55dabb
Ashish Yadav <ashishyadav@Ashishs-MacBook-Air.local>
2023-12-25 22:06:24 +0530

    merged all master changes into main

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 const zod = require('zod');
 
+const {pool} = require('../config')
+
 const createUserSchema = zod.object({
   username: zod.string(),
   password: zod.string(),
-  dob: zod.string(),
+  dateofbirth: zod.string(),
   firstName: zod.string(),
   lastName: zod.string(),
+  email: zod.string()
 });
 
 const numberSchema = zod.string();
@@ -16,10 +19,21 @@ const users = [];
 
 // GET /users
 router.get('/', (req, res) => {
-  res.status(200).json({ users, message: 'List of users', success: true });
-});
 
-// GET /users/:id
+  const insertQuery = 'select * from users'
+
+  pool.query(insertQuery, (error, result) => {
+    if (error) {
+      console.error('Error receieving user details:', error);
+    } else {
+       const allUsers = result.rows;
+       res.status(201).json({ allUsers, message: 'All Users get successfully', success: true });
+    }
+  })
+ });
+
+
+/* Get request to get the user detail */
 router.get('/:userId', (req, res) => {
   const result = numberSchema.safeParse(req.params.userId)
   const user = users.find((u) => u.id === result.data);
@@ -31,18 +45,30 @@ router.get('/:userId', (req, res) => {
   res.status(200).json({ user, message: 'User details', success: true });
 });
 
-// POST /users
-router.post('/', (req, res) => {
-  const { value, error } = createUserSchema.safeParse(req.body);
+/*  Post request for users */
+router.post('/', async (req, res) => {
+  const response = createUserSchema.safeParse(req.body);
 
-  if (error) {
+  const {data} = response
+  if (!response.success) {
     return res.status(400).json({ message: 'Invalid request data', success: false, error });
   }
 
-  const newUser = { id: Date.now().toString(), ...value };
-  users.push(newUser);
+  const insertQuery = 'INSERT INTO users (username, password, dateofbirth, firstname, lastname, email) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
+  const values = [data.username, data.password, data.dateofbirth, data.firstName, data.lastName, data.email];
 
-  res.status(201).json({ user: newUser, message: 'User created successfully', success: true });
+
+  pool.query(insertQuery, values, (error, result) => {
+    if (error) {
+      console.error('Error inserting user details:', error);
+    } else {
+      const insertedUser = result.rows[0];
+      console.log('User inserted successfully:', insertedUser);
+      res.status(201).json({ user: insertedUser, message: 'User created successfully', success: true });
+
+    }
+  })
 });
+
 
 module.exports = router;
