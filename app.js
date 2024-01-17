63dbfb2523a3a038e1c53be8e7a1c6b54d6364c8
Ashish Yadav <ashishyadav@Ashishs-MacBook-Air.local>
2024-01-17 23:40:14 +0530

    used nodeman package to hot reload

--- app.js
+++ app.js
@@ -51,7 +51,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
  
-
 app.use('/users', usersRouter);
 app.use('/', indexRouter);
 
